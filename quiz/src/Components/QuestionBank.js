2444a63721f6789c29366e6736fbbe21ade2166b
Maximus Harding <maximusharding@gmail.com>
2024-10-22 16:23:47 -0500

    added quiz from tutorial

---
+++ quiz/src/Components/QuestionBank.js
@@ -0,0 +1,18 @@
+const qBank = [
+    {
+        id: 1,
+        image: null,
+        question: "What do Rolls-Royce and BMW have in common?",
+        options: ["Both are owned by BMW", "Both are luxury brands", "Both are Japanese", "They have nothing in common"],
+        answer: "Both are owned by BMW",
+    },
+    {
+        id: 2,
+        image: null,
+        question: "What does BMW stand for?",
+        options: ["Bad Money Works", "Bay Motor Works", "Bayerische Motoren Werke", "Three names: Bob, Max, and Wyatt"],
+        answer: "Bayerische Motoren Werke",
+    },
+]
+
+export default qBank;
