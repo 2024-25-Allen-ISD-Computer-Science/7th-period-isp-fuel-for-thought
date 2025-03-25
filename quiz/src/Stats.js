e6c95ea95f25949feaf6108b93fea1c991011d9d
Maximus Harding <maximusharding@gmail.com>
2025-03-25 14:02:21 -0500

    added more stats in the stats page

--- quiz/src/Stats.js
+++ quiz/src/Stats.js
@@ -3,6 +3,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class Stats extends Component {
+    constructor() {
+        super()
+        this.correct = localStorage.getItem("totalRight") ?? 0
+        this.incorrect = localStorage.getItem("totalWrong") ?? 0
+    }
 
     render() {
         const { isPresented } = this.props;
@@ -13,8 +18,11 @@ class Stats extends Component {
                     <button className="btn btn-primary" onClick={isPresented}>Back</button>
                 </div>
                 <div className="d-flex flex-column align-items-center justify-content-center">
-                    <div className="results-title">Total Questions Correct: {localStorage.getItem("totalRight") ?? 0}</div>
-                    <div className="results-title">Total Questions Incorrect: {localStorage.getItem("totalWrong") ?? 0}</div>
+                    <div className="results-title">Total Questions Correct: {this.correct}</div>
+                    <div className="results-title">Total Questions Incorrect: {this.incorrect}</div>
+                    <br/>
+                    <div className="results-title">Percent Correct: {parseFloat(this.correct / this.incorrect).toFixed(2)}%</div>
+                    <div className="results-title">Total Questions Answered: {Number(this.correct) + Number(this.incorrect)}</div>
                 </div>
             </div>
         )
